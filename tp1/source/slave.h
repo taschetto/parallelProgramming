031b798d5f43c5e30d7a24c13cbb62ddadeab865
Guilherme Taschetto <gtaschetto@gmail.com>
2015-09-28 11:09:52 -0300

    Update slave.h

--- tp1/source/slave.h
+++ tp1/source/slave.h
@@ -11,8 +11,8 @@ public:
   void mainLoop();
 
 private:
-  int _rank;     // rank do processo
-  int _job_size; // tamanho de cada job
+  int rank;     // rank do processo
+  int job_size; // tamanho de cada job
   void receiveJob(int*, MPI_Status*); // recebe um job do master
   void doJob(int*); // executa o job
   void sendResultsToMaster(int*); // devolve o resultado para o master
